54a77ecfb37c2f74341fced02ae8da5169f5e593
chaser <1071070045244@qq.com>
2019-08-07 15:39:36 +0800

    add node config

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -68,7 +68,20 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: ['file-loader']
       }
-    ]
+    ],
+
+    node: {
+      // prevent webpack from injecting useless setImmediate polyfill because Vue
+      // source contains it (although only uses it if it's native).
+      setImmediate: false,
+      // prevent webpack from injecting mocks to Node native modules
+      // that does not make sense for the client
+      dgram: 'empty',
+      fs: 'empty',
+      net: 'empty',
+      tls: 'empty',
+      child_process: 'empty'
+    }
   },
 
   plugins: [
