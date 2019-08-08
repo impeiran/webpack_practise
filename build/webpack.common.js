f639acf0f77fe6d015da90433e67ee1838c19ecf
chaser-gosuncn <1070045244@qq.com>
2019-08-08 10:16:18 +0800

    remove node modules

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -3,8 +3,6 @@ const webpack = require('webpack')
 
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlPlugin = require('html-webpack-plugin')
-const MinifyPlugin = require("babel-minify-webpack-plugin");
-
 
 module.exports = {
   context: path.resolve(__dirname, '../'),
@@ -16,7 +14,7 @@ module.exports = {
 
   output: {
     path: path.resolve(__dirname, '../dist'),
-    filename: '[name].[hash:7].js'
+    filename: '[name].js'
   },
 
   module: {
@@ -59,7 +57,7 @@ module.exports = {
 
       // media loader
       {
-        test: /\.(jpg|png|svg|gif|mp4|mp3)$/,
+        test: /\.(jpg|png|svg|gif)$/,
         use: ['file-loader']
       },
 
@@ -68,20 +66,7 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: ['file-loader']
       }
-    ],
-
-    node: {
-      // prevent webpack from injecting useless setImmediate polyfill because Vue
-      // source contains it (although only uses it if it's native).
-      setImmediate: false,
-      // prevent webpack from injecting mocks to Node native modules
-      // that does not make sense for the client
-      dgram: 'empty',
-      fs: 'empty',
-      net: 'empty',
-      tls: 'empty',
-      child_process: 'empty'
-    }
+    ]
   },
 
   plugins: [
@@ -92,8 +77,6 @@ module.exports = {
     new HtmlPlugin({
       template: path.resolve(__dirname, '../src/index.html'),
       minify: true
-    }),
-    // new MinifyPlugin(),
-
+    })
   ]
 }
